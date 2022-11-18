aaa38d13ee70b9716840fdd73c460c1e2d384fe6
John Johnson <jjmbeba@gmail.com>
2022-11-18 22:28:43 +0300

    fix menu

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -6,7 +6,7 @@ return (
   <ul className='flex flex-col gap-[25px]'>
       {
           menu.map((item) => (
-              <li key={item} className={`text-[14px] leading-[20px] font-[500] text-[#9E9E9E] font-noto ${item === "Inputs" && 'font-bold text-[#090F31]'}`}>{item}</li>
+              <li key={item} className={`text-[14px] leading-[20px] font-[500] text-[#9E9E9E] font-noto ${item === "Buttons" && 'font-bold text-[#090F31]'}`}>{item}</li>
           ))
       }
   </ul>
